3a88678e0be02202cc1010e7a72c4e3fc1943d64
1831127 <1831127@LABBMAQ14.itp.local>
2019-04-23 11:51:10 -0300

    formulario para ususario

---
+++ visao/usuario/login.visao.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    <title>Formulario</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div id="geral">
+      <div id="tudo">
+        <h1>Bem vindo!</h1>
+        <p>
+        Formulario 
+        </p>
+        <p>
+          Login Cadastro
+        </p> 
+        <table id="tabela">
+          <tr>
+            <td>Nome:</td>
+            <td > <input type="text" name="nome" size="25" id="cadastro" id="pro"> </td>
+          </tr>
+          <tr>
+            <td>Email:</td>
+            <td><input type="text" name="email" size="25"></td>
+          </tr>
+          <tr>
+            <td>Senha:</td>
+            <td><input type="text" name="senha" size="25"></td>
+          </tr>
+          <tr>
+            <td>Confirme a Senha:</td>
+            <td><input type="text" name="confirme" size="25"></td>
+          </tr>
+          <tr>
+              <td><button type="submit">Login</button></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </body>
+</html>
+
