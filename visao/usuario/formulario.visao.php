f1b7e735389ca719171106d24bf45b9d91356def
1831127 <1831127@LABBMAQ14.itp.local>
2019-04-23 11:25:55 -0300

    formulario

--- visao/usuario/formulario.visao.php
+++ visao/usuario/formulario.visao.php
@@ -1,7 +1,7 @@
 <form action="" method="POST">
     nome: <input type="text" name="nome" value="<?=@$usuario['nome']?>">
     email: <input type="text" name="email" value="<?=@$usuario['email']?>">
-    produto: <input type="text" name="email" value="<?=@$usuario['produto']?>">
+    produto: <input type="text" name="produto" value="<?=@$usuario['produto']?>">
     senha: <input type="password" name="senha" value="<?=@$usuario['senha']?>">
     <button type="submit">Enviar</button>
 </form>
