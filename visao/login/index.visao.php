cb545fe5b4e9c0501503886d87322609270440b4
1831321 <1831321@LABBMAQ05.itp.local>
2019-06-07 08:56:47 -0300

    adicionar formulario

--- visao/login/index.visao.php
+++ visao/login/index.visao.php
@@ -3,12 +3,27 @@
 <form method="POST" action="">
 
 
-    <label for="login">email:</label>
+    <label for="passwd">Nome:</label>
+    <input id="passwd" class="form-control" type="password" name="nome">
+    
+    <label for="login">Email:</label>
     <input id="login" class="form-control" type="text" name="email">
 
-    <label for="passwd">senha:</label>
+    <label for="passwd">Senha:</label>
     <input id="passwd" class="form-control" type="password" name="senha">
+    
+    <label for="passwd">CPF:</label>
+    <input id="passwd" class="form-control" type="password" name="cpf">
 
+    <label for="passwd">Data de Nascimento:</label>
+    <input id="passwd" class="form-control" type="password" name="datadenascimento">
+
+    <label for="passwd">Sexo:</label>
+    <input id="passwd" class="form-control" type="password" name="sexo">
+    
+    <label for="passwd">Tipo de Usuário:</label>
+    <input id="passwd" class="form-control" type="password" name="tipousuario">
+    
     <button type="submit" class="btn btn-primary">vai login</button>
 
 </form>
